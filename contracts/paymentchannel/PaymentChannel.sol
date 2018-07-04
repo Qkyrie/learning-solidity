1987d07e588eff0ae1c7a359dcd8812d0b7b2648
Quinten De Swaef <quintendeswaef@gmail.com>
2018-07-04 14:33:49 +0200

    add payment channel

--- contracts/paymentchannel/PaymentChannel.sol
+++ contracts/paymentchannel/PaymentChannel.sol
@@ -1,21 +1,28 @@
 pragma solidity ^0.4.24;
 
- /*
-  * Reference implementation of a payment channel.
-  * @author Quinten De Swaef
-  */
+/*
+ * Reference implementation of a payment channel.
+ * @author Quinten De Swaef
+ */
 contract PaymentChannel {
 
-    constructor() {
+    address public receiver;
+    address public sender;
+    uint public deadline;
 
+    constructor(address _sender, address _receiver, uint _deadline) {
+        receiver = _receiver;
+        sender = _sender;
+        deadline = _deadline;
     }
 
     function submitProof() external returns (bool) {
         return true;
     }
 
-
     function close() external returns (bool) {
+        require(now > deadline, "deadline has not been reached yet");
+        require(sender.send(address(this).balance));
         return true;
     }
 }
