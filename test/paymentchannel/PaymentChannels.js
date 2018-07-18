afb6ebbc8af6331cf82cd3732d1ff4923cd7b80b
Quinten De Swaef <quintendeswaef@gmail.com>
2018-07-18 17:03:52 +0200

    add tests

---
+++ test/paymentchannel/PaymentChannels.js
@@ -0,0 +1,9 @@
+const PaymentChannel = artifacts.require("./paymentchannel/PaymentChannel.sol")
+
+import chai from 'chai';
+const expect = chai.expect;
+
+
+contract('PaymentChannel', function(accounts) {
+
+})
