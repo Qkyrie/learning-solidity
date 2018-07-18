f51b5abcaafd00b54194b94f774b9f0e29a08515
Quinten De Swaef <quintendeswaef@gmail.com>
2018-07-18 16:42:01 +0200

    put them in sync " "

--- truffle.js
+++ truffle.js
@@ -10,15 +10,6 @@ require('babel-register')({
 require('babel-polyfill');
 
 
-
-const mochaGasSettings = {
-  reporter: 'eth-gas-reporter',
-  reporterOptions : {
-    currency: 'USD',
-    gasPrice: 3
-  }
-}
-
 const mocha = process.env.GAS_REPORTER ? mochaGasSettings : {};
 
 module.exports = {
@@ -27,29 +18,13 @@ module.exports = {
 			network_id: '*',
 			host: "localhost",
 			port: 8545,
-		},
-		staging: {
-			network_id: '42',
-			provider: new HDWalletProvider(process.env.MNEMONIC, "https://kovan.fundrequest.io/")
-		},
-		test: {
-			network_id: '42',
-			provider: new HDWalletProvider(process.env.MNEMONIC, "https://kovan.fundrequest.io/")
-		},
-		mainnet: {
-			network_id: '1',
-      provider: new HDWalletProvider(process.env.MNEMONIC_MAINNET, "https://mainnet.fundrequest.io"),
-			from: process.env.MAINNET_FROM,
-			gas: 4612388,
-			gasPrice: 23000000000
-		},
-    coverage: {
-      host: "localhost",
-      network_id: "*",
-      port: 8555,
-      gas: 0xffffffffff,
-      gasPrice: 0x01
-    }
+		}
 	},
-	mocha,
+	mocha: {
+		reporter: 'eth-gas-reporter',
+		reporterOptions : {
+			currency: 'USD',
+			gasPrice: 3
+		}
+	}
 };
